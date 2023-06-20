075e00131206ff7024487a117e10c3a5af0643c1
Yrobot <Yrobot@qq.com>
2023-06-20 22:28:38 +0800

    init basic api

---
+++ app/api/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return new NextResponse("Hello!!11", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
